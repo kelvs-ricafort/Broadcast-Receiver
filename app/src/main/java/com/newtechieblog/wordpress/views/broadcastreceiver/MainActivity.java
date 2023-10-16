85c3c22ce294fad9765097945109eaf5f86e95ee
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-16 16:46:43 +0800

    Intent based Broadcast Receivers

--- app/src/main/java/com/newtechieblog/wordpress/views/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/broadcastreceiver/MainActivity.java
@@ -2,13 +2,35 @@ package com.newtechieblog.wordpress.views.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    BroadcastExample br = new BroadcastExample();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(br, filter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(br, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.unregisterReceiver(br);
     }
 }
